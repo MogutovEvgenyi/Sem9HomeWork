74c894ed2bbede5782d0bf5d428f9aefeded61a6
MogutovEvgenyi <fantast08@mail.ru>
2023-09-20 23:37:16 +0500

    решение

---
+++ Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumNaturalElements(m, n);
+
+void SumNaturalElements(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
