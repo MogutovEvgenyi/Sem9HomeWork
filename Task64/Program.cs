6eacd563dc1517fd7ffd99c8bfd7084c36d7a24e
MogutovEvgenyi <fantast08@mail.ru>
2023-09-20 22:48:57 +0500

    решение

---
+++ Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿int InputNum(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine()!);
+}
+void PrintNumbers(int n)
+{
+    if (n > 0)
+    {
+        Console.Write(n + " ");
+        PrintNumbers(n - 1);
+
+    }
+}
+//
+int number = InputNum("Введите число: ");
+PrintNumbers(number);
